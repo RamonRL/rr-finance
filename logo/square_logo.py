9927f378a4ea88500db25bb0a372f42b879170a7
Ramon Romero <ramon.romero@unnax.com>
2026-05-05 14:50:08 +0200

    logo

--- logo/square_logo.py
+++ logo/square_logo.py
@@ -5,10 +5,15 @@ HERE = Path(__file__).parent
 SOURCE = HERE / "newlogo.png"
 
 
+MARGIN_RATIO = 0.08
+
+
 def square_with_background(src: Path, bg: tuple[int, int, int]) -> Image.Image:
     img = Image.open(src).convert("RGBA")
     w, h = img.size
-    side = max(w, h)
+    inner = max(w, h)
+    margin = round(inner * MARGIN_RATIO)
+    side = inner + 2 * margin
 
     canvas = Image.new("RGBA", (side, side), (*bg, 255))
     offset = ((side - w) // 2, (side - h) // 2)
